de9e3b3af8b8f3d9a335ad17d52fc50b186035cd
Clara Helena <clarahelenasena@gmail.com>
2024-07-10 08:50:52 -0400

    add html tags and js changes

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-const section = document.querySelector("section");
+const formatDate = (data) => {
+  return {
+    dia: {
+      numerico: dayjs(data).format('DD'),
+      semana: {
+        curto: dayjs(data).format('ddd'),
+        longo: dayjs(data).format('dddd'),
+      }
+    },
+    mes: dayjs(data).format('MMMM'),
+    hora: dayjs(data).format('HH:mm')
+  }  
+}
 
 const atividade = {
-  nome: "lunch",
+  nome: "Lunch Time",
   data: new Date("2024-07-09 12:00"),
   finalizada: true,
 };
 
-const activities = [
-  atividade,
-  {
-    nome: "gaming session",
-    data: new Date("2024-07-09 17:00"),
-    finalizada: true,
-  },
-  {
-    nome: "sleep time",
-    data: new Date("2024-07-09 22:00"),
-    finalizada: true,
-  },
-];
+let activities = [atividade];
 
 const createActivityItem = (atividade) => {
   let input = '<input type="checkbox" ';
-
+  
   if (atividade.finalizada) {
     input += "checked";
   }
-
+  
   input += ">";
 
+  const dateFormated = formatDate(atividade.data);
+  
   return `
   <div>
-    ${input}
-    <span>${atividade.nome}</span>
-    <span>${atividade.data}</span>
+  ${input}
+  <span>${atividade.nome}</span>
+  <span>${dateFormated.dia.semana.longo}, dia ${dateFormated.dia.numerico} de ${dateFormated.mes} às ${dateFormated.hora}</span>
   </div>
   `;
 };
 
-for (let atividade of atividades) {
-  section.innerHTML += createActivityItem(atividade);
+const updateActivityList = () => {
+  const section = document.querySelector("section");
+
+  if(activities.length == 0) {
+    section.innerHTML = '<p>Nenhuma atividade cadastrada.</p>'
+    return
+  }
+
+  for (let atividade of activities) {
+    section.innerHTML += createActivityItem(atividade);
+  }
+
 }
+
+updateActivityList();
